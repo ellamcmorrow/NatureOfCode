828dfa5c611d6f351d790a36838d55aa5a57f0d3
Admin <admin@HP-C4580-MacBook-Air.local>
2018-01-16 12:59:34 +0000

    Car moving to the right

---
+++ sketch01.js
@@ -0,0 +1,36 @@
+
+
+//defining the vars
+var myCar;
+
+//static function, called once when the page/app loads
+function setup(){
+	createCanvas(320,640);
+	background(127);
+	myCar= new Car();
+	console.log(myCar);
+}
+
+//loop function called each frame eg. 30 frames per sec = 30 calls
+function draw(){
+	myCar.display();
+	myCar.drive();
+
+}
+
+
+function Car (){
+	
+	this.color= 233;
+	this.xpos = width/2;
+
+
+	this.display = function(){
+		fill(this.color);
+		rect(this.xpos,height/2,1,1);
+	}
+
+	this.drive=function(){
+		this.xpos=this.xpos+1;
+	}
+}
