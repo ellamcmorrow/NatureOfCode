2a0f1db30cdb127e0129afadb4ab4328aaeeee3a
Admin <admin@HP-C4580-MacBook-Air.local>
2018-01-21 17:54:49 +0000

    using random to manipulate pixels and make fuzzyness

---
+++ sketch12.js
@@ -0,0 +1,28 @@
+
+//static function called once when page loads
+function setup(){
+	createCanvas(1200,1000);
+	pixelDensity(1); //necessary to fix any HD retina display issues 
+}
+
+//loop function called each frame eg. 30 frames per sec = 30 calls
+function draw(){
+
+
+	loadPixels(); //load pixels i.e i wanna work with pixels
+	for(y =0; y<height; y++){ //creating a nexted for loop to manipulate the x and y pixels
+	for(x =0; x<width; x++){ //looping through each x,y value
+
+			var index= (x+y*width)*4; //formula to manipulate x,y's
+			var brightness=random(255);
+
+	/*R*/	pixels[index+0]=brightness; //change each pixel array value
+	/*G*/	pixels[index+1]=brightness;
+	/*B*/	pixels[index+2]=brightness;
+	/*A*/	pixels[index+3]=255; 
+}
+//fuzzyness created
+}
+	updatePixels(); //apply to canvas
+
+}
