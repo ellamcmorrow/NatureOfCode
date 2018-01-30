4a8f8ef16e388a9afea3fd9c4ac42623bcb3c6be
Admin <admin@HP-C4580-MacBook-Air.local>
2018-01-30 10:13:39 +0000

    read me

--- sketch12.js
+++ sketch12.js
@@ -16,10 +16,10 @@ function draw(){
 			var index= (x+y*width)*4; //formula to manipulate x,y's
 			var brightness=random(255);
 
-	/*R*/	pixels[index+0]=brightness; //change each pixel array value
-	/*G*/	pixels[index+1]=brightness;
-	/*B*/	pixels[index+2]=brightness;
-	/*A*/	pixels[index+3]=255; 
+	/*R*/	pixels[index+0]=y/2; //change each pixel array value
+	/*G*/	pixels[index+1]=brightness/2;
+	/*B*/	pixels[index+2]=brightness;//brightness;
+	/*A*/	pixels[index+3]=x; 
 }
 //fuzzyness created
 }
